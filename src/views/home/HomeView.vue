0bf10c9ae6740320981926426cdaeaf5748b3086
Jasa Plohl <jasa.plohl@gmail.com>
2023-06-14 15:00:56 +0200

    About me section

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -4,11 +4,13 @@ import FooterComponent from "@/components/FooterComponent.vue";
 import ProjectsComponent from "@/views/home/components/ProjectsComponent.vue";
 import TechnologiesComponent from "@/views/home/components/TechnologiesComponent.vue";
 import ContactComponent from "@/views/home/components/ContactComponent.vue";
+import AboutMeComponent from "@/views/home/components/AboutMeComponent.vue";
 </script>
 
 <template>
   <main>
     <IntroductionComponent />
+    <AboutMeComponent />
     <ProjectsComponent />
     <TechnologiesComponent />
     <ContactComponent />
---
+++ src/views/home/components/AboutMeComponent.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+// TODO: get everything on the same page without scrolling.
+// TODO: View more -> router for the subpages (About me, projects ...)
+// TODO: ChatGPT for my introduction
+</script>
+
+<template>
+  <div class="page">
+    <h1>About me</h1>
+    <div class="page--center">
+      <p>
+        Welcome to my portfolio! My name is Jaša Plohl. I'm a full stack developer based in Kranj, Slovenia.
+      </p>
+      <img alt="Me" src="/images/me.jpg" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+img {
+  max-width: 15rem;
+  border-radius: 50%;
+}
+</style>
