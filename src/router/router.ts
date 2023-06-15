b54e1c89820c57513b8b15852978b9a75d3e4be1
Jasa Plohl <jasa.plohl@gmail.com>
2023-06-15 07:59:02 +0200

    Project structure.

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,35 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/home/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      children: [
-        {
-          path: '',
-          component: () => import('@/views/home/views/IntroductionComponent.vue'),
-        },
-        {
-          path: 'about',
-          component: () => import('@/views/home/views/AboutMeComponent.vue'),
-        },
-        {
-          path: 'projects',
-          component: () => import('@/views/home/views/ProjectsComponent.vue'),
-        },
-        {
-          path: 'technologies',
-          component: () => import('@/views/home/views/TechnologiesComponent.vue'),
-        },
-        {
-          path: 'contact',
-          component: () => import('@/views/home/views/ContactComponent.vue'),
-        }
-      ]
+      path: '',
+      component: () => import('@/views/introduction/IntroductionView.vue'),
+    },
+    {
+      path: '/about',
+      component: () => import('@/views/about/AboutView.vue'),
+    },
+    {
+      path: '/projects',
+      component: () => import('@/views/projects/ProjectsView.vue'),
+    },
+    {
+      path: '/technologies',
+      component: () => import('@/views/technologies/TechnologiesView.vue'),
+    },
+    {
+      path: '/contact',
+      component: () => import('@/views/contact/ContactComponent.vue'),
     }
   ]
 })
