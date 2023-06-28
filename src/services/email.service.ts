e7bcae000c77ab595d8755757a42e9bb83b7a831
Jasa Plohl <jasa.plohl@devleet.si>
2023-06-28 07:26:23 +0200

    Removed the API key from the code.

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,19 +1,22 @@
 import * as client from '@sendgrid/mail';
 import type { MailDataRequired } from '@sendgrid/helpers/classes/mail';
+import {loadEnv} from "vite";
 
 export const sendEmail = async (fromAddress: string, subject: string, message: string): Promise<void> => {
-    client.setApiKey('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');
-    const emailData: MailDataRequired = {
-        from: fromAddress,
-        to: 'jasa.plohl@gmail.com',
-        subject: subject,
-        text: message,
-        html: `<p>${message}</p>` // TODO: HTML template
-    };
-    try {
-        const res = await client.send(emailData);
-        console.log(res);
-    } catch(err) {
-        console.error(err);
-    }
+    // console.log(process.env.SEND_GRID_API_KEY);
+    // console.log(process.env.VUE_APP_TEST);
+    // client.setApiKey(process.env.);
+    // const emailData: MailDataRequired = {
+    //     from: fromAddress,
+    //     to: 'jasa.plohl@gmail.com',
+    //     subject: subject,
+    //     text: message,
+    //     html: `<p>${message}</p>` // TODO: HTML template
+    // };
+    // try {
+    //     const res = await client.send(emailData);
+    //     console.log(res);
+    // } catch(err) {
+    //     console.error(err);
+    // }
 };
